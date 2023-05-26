228bc476b4a6e55ddedda8de6f8c77ef4c2da77e
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-26 18:41:47 +0300

    task 3

--- script.js
+++ script.js
@@ -428,3 +428,40 @@ document.querySelector('h2').addEventListener('click', function () {
   document.querySelector('h2').style.color = 'red';
   console.log('asdasd');
 });
+
+// TASK 3 ______________________________________________________________
+
+const events = new Map([
+  [19, 'Goal'],
+  [21, 'Substitution'],
+  [43, 'Goal'],
+  [56, 'Substitution'],
+  [69, 'Yellow card'],
+  [73, 'Substitution'],
+  [75, 'Yellow card'],
+  [79, 'Substitution'],
+  [81, 'Red card'],
+  [93, 'Goal'],
+]);
+
+// 1
+
+const gameEvents = [...new Set(events.values())];
+console.log(gameEvents);
+
+// 2
+
+events.delete(75);
+console.log(events);
+
+//3
+
+console.log(90 / events.size);
+
+//4
+
+for (const [timeEvents, event] of events) {
+  timeEvents < 45
+    ? console.log(`[FIRSTHALF] ${timeEvents}: ${event}`)
+    : console.log(`[SECONDHALF] ${timeEvents}: ${event}`);
+}
